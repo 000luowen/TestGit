8c4a8e2fa80949ff90f07cbccc2d457d9541754f
luowen <905558614@qq.com>
2017-10-13 22:25:16 +0800

    第二次修改MainActivity中onCreate()

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //第一次修改MainActivity的onCreate();
+        //第二次修改MainActivity的onCreate();
     }
 }
